666759252b21a963174516a791ce31656dff536d
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-19 15:46:56 +0200

    added event policy

---
+++ app/Policies/EventPolicy.php
@@ -0,0 +1,20 @@
+<?php
+
+// app/Policies/EventPolicy.php
+namespace App\Policies;
+
+use App\Models\Event;
+use App\Models\User;
+
+class EventPolicy
+{
+    public function update(User $user, Event $event)
+    {
+        return $user->id === $event->user_id || $user->role === 'admin';
+    }
+
+    public function delete(User $user, Event $event)
+    {
+        return $user->id === $event->user_id || $user->role === 'admin';
+    }
+}
