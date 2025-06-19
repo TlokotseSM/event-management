4afacaf815972e7c6302d18d871f006dc019fbb7
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-19 16:12:19 +0200

    updated api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\EventController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register']);
@@ -14,4 +15,8 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/events/{event}/register', [EventController::class, 'register']);
     Route::delete('/events/{event}/cancel', [EventController::class, 'cancelRegistration']);
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
