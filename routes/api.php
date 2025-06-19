ae0a95385199bec33ef0e8ceb74581e3ddf7111f
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-19 15:29:01 +0200

    added routes api

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\EventController;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('events', EventController::class);
+
+    Route::post('/events/{event}/register', [EventController::class, 'register']);
+    Route::delete('/events/{event}/cancel', [EventController::class, 'cancelRegistration']);
+});
