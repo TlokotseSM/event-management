4306b0ef11ce9ed4e378741dc933dc2f3f778168
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-20 10:43:04 +0200

    updated category migration table to  include the slug column that my CategoryFactory is trying to populate

--- database/migrations/0001_01_01_000004_create_categories_table.php
+++ database/migrations/0001_01_01_000004_create_categories_table.php
@@ -6,23 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('slug')->unique(); // Add this line
             $table->text('description')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('categories');
     }
