f9f1a4e3bf2bc7e47f8f23a816a9d8ea620f7e4d
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-20 09:19:43 +0200

    updated Controller.php base class with all necessary traits for my Event Management API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,45 @@
 
 namespace App\Http\Controllers;
 
-abstract class Controller
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
 {
-    //
+    use AuthorizesRequests, ValidatesRequests;
+
+    /**
+     * Success response method
+     */
+    public function sendResponse($result, $message = '', $code = 200)
+    {
+        $response = [
+            'success' => true,
+            'data'    => $result,
+        ];
+
+        if (!empty($message)) {
+            $response['message'] = $message;
+        }
+
+        return response()->json($response, $code);
+    }
+
+    /**
+     * Error response method
+     */
+    public function sendError($error, $errorMessages = [], $code = 404)
+    {
+        $response = [
+            'success' => false,
+            'message' => $error,
+        ];
+
+        if (!empty($errorMessages)) {
+            $response['errors'] = $errorMessages;
+        }
+
+        return response()->json($response, $code);
+    }
 }
