f22a458689ee2279c077d5e9683f45586c8f7388
TlokotseSM <tlokotsemogudi@gmail.com>
2025-06-20 12:34:57 +0200

    updated the attendee model

--- app/Models/Attendee.php
+++ app/Models/Attendee.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class Attendee extends Model
+class Attendee extends Pivot
 {
     use HasFactory;
 
@@ -16,6 +17,8 @@ class Attendee extends Model
         'event_id',
         'status' // ['registered', 'attended', 'cancelled']
     ];
+
+    public $incrementing = true;
 
     protected $casts = [
         'attended_at' => 'datetime',
